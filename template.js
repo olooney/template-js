89f084d5eb8eeb1c1987a03d3007bd0202c76144
Oran Looney <olooney@gmail.com>
2011-02-28 21:56:45 -0600

    now compiling each block to a separate method to prepare for template inheritance.

--- template.js
+++ template.js
@@ -23,8 +23,8 @@ function compileText(src) {
 // add parens and squigly brackets as needed to block constructs.
 var autoBracket = (function() {
 	var known = [
-		[/^\s*block\s.*$/, ''], // TODO
-		[/^\s*endblock.*$/, ''], // TODO
+		[/^\s*block\s+(\w+)\s*$/, 'block $1'],
+		[/^\s*endblock\s+(\w*)\s*$/, 'endblock $1'],
 		[/^\s*if\s*(.*)$/, 'if($1){'],
 		[/^\s*else\s+if\s*(.*)$/, '}else if($1){'],
 		[/^\s*else/, '}else{'],
@@ -47,30 +47,87 @@ var autoBracket = (function() {
 	}
 })();
 
+function Frame(name) {
+	this.name = name;
+	this.lines = ['var lines=[];with(context||{}){'];
+}
+Frame.prototype = {
+	constructor: Frame,
+	add: function(line) {
+		this.lines.push(line);
+	},
+	compile: function() {
+		this.lines.push(" } return lines.join('');");
+		var fn = new Function('context', this.lines.join(''));
+		this.lines.pop();
+		return fn;
+	}
+};
 
 function compile(src) {
 	var pattern = /(\{%(.*?)%\})/gm;
 	var match = pattern.exec(src);
 	var lastEnd = 0;
 	
-	var pieces = ['var lines=[];with(arguments[0]||{}){'];
+	var stack = [ new Frame('root') ];
+	var blocks = {};
+
+	function top() { return stack[stack.length-1]; }
 
 	function pushText(text) { 
 		if ( !text ) return;
-		pieces.push('lines.push(');
-		pieces.push( compileText(text) );
-		pieces.push(');');
+		top().add('lines.push(');
+		top().add( compileText(text) );
+		top().add(');');
 	}
 
 	while ( match ) {
+		// take everything to up but not including the matched block tag.
 		pushText(src.slice(lastEnd, match.index));
-		var block = match[2];
-		pieces.push(  autoBracket(block) );
+
+		var tag = autoBracket(match[2]);
+		var block = (/^block (\w+)$/).exec(tag);
+		var endblock = (/^endblock (\w*)$/).exec(tag);
+		if ( block ) {
+			var blockName = block[1];
+			console.log('block', blockName);
+			// todo: throw if duplicate?
+			top().add('lines.push(this.' + blockName + '(context));');
+			stack.push( new Frame(blockName) );
+		} else if ( endblock ) {
+			var blockName = endblock[1];
+			console.log('endblock', blockName);
+			if ( blockName === '' || blockName === top().name ) {
+				blocks[ top().name ] = top().compile();
+				stack.pop();
+			} else {
+				throw new Error("mismatched endblock " + blockName + " in block " + top().name);	
+			}
+		} else {
+			// treat the tag as code.  Brackets have already been added if needed.
+			stack[stack.length-1].add(tag);
+		}
 		lastEnd = match.index + match[0].length;
 		match = pattern.exec(src);
 	}
 	pushText(src.slice(lastEnd));
+	
+	if ( stack.length !== 1 ) {
+		throw new Error("unclosed blocks!");
+	}
+	blocks.root = stack[0].compile();
 
-	pieces.push(" } return lines.join('');");
-	return new Function(pieces.join(''));
+	function template(context) {
+		return arguments.callee.blocks.root(context);
+	}
+	template.blocks = blocks;
+	template.toCodeString = function() {
+		var blocks = this.blocks;
+		var methods = [];
+		for ( var name in blocks ) {
+			methods.push(name + ': ' + blocks[name]);
+		}
+		return 'Template({' + methods.join(',') + '})';
+	}
+	return template;
 }
